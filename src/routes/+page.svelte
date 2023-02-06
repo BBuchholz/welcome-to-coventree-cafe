7d4a4c0fb5710f9427b9040beee176c2e575350f
Brent Buchholz <brentbuchholz@gmail.com>
2023-02-06 11:42:44 -0600

    Add Foundling Layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
- 
-<p>TODO: copy over the template currently residing in tempTemplateFolder into the appropriate places for sveltekit</p>
+<script>
+   import FoundlingLayout from "$lib/FoundlingLayout.svelte";
+</script>
+
+<div class="main">
+    <FoundlingLayout />
+</div>
